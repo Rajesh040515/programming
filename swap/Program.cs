9367aca8079d4dbf002e10efc19acb0b7e57b22b
Rajesh <rajesh.facile@outlook.com>
2021-03-23 21:25:57 -0400

    factorial number

---
+++ swap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, f = 1;
+            for( i=1; i <= 5; i++)
+            {
+                f = f * i;
+                Console.WriteLine(f);
+
+            }
+        }
+    }
+}
