ab3653ae227055d8543360c197fbfe5120beb949
Rajesh <rajesh.facile@outlook.com>
2021-04-02 21:30:33 -0400

    Square Root Program

---
+++ Square Root/square root/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace square_root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter  Number: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            var squar = new square().sqrt(num);
+            Console.WriteLine(squar);
+        }
+    }
+}
+public class square
+{
+    public int sqrt(int n)
+    {
+        return n * n;
+    }
+}
