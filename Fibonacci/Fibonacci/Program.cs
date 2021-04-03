0222e8e3411272b1b0f930307996b73dc58b0542
Rajesh <rajesh.facile@outlook.com>
2021-04-02 21:37:13 -0400

    fibonacci program

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  a = 0, b = 1, c;
+
+            for(int i=0;i<5;i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
